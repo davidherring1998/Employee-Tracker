7f0f7534c5255a406a686796e3b6a772a378ec7d
David Herring <davidherring@Davids-MBP.home>
2022-11-16 14:31:45 -0600

    update

--- index.js
+++ index.js
@@ -2,46 +2,39 @@ const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 const { connect } = require("./config/connection");
 
-// const works = () => {
-//   console.log("working");
-// };
-
 function main() {
   inquirer
     .prompt({
       name: "initialPage",
       type: "list",
       choices: [
-        "view all departments",
-        "view all employees",
-        "view all roles",
-        "add a department",
-        "add a employee",
-        "add a role",
-        "update existing employee role",
-        "main view",
+        "View all departments",
+        "View all employees",
+        "View all roles",
+        "Add a department",
+        "Add a employee",
+        "Add a role",
+        "Update existing employee role",
       ],
     })
     .then((res) => {
       const answer = res.initialPage;
-      if (answer === "view all departments") {
+      if (answer === "View all departments") {
         viewDepartments();
-      } else if (answer === "view all employees") {
+      } else if (answer === "View all employees") {
         viewEmployees();
-      } else if (answer === "view all roles") {
+      } else if (answer === "View all roles") {
         viewRoles();
-      } else if (answer === "add a department") {
+      } else if (answer === "Add a department") {
         addDepartment();
-      } else if (answer === "add a employee") {
+      } else if (answer === "Add a employee") {
         addEmployee();
-      } else if (answer === "add a role") {
+      } else if (answer === "Add a role") {
         addRoles();
-      } else if (answer === "update existing employee role") {
+      } else if (answer === "Update existing employee role") {
         updateEmployeeRole();
-      } else if (answer === "main view") {
-        main();
       } else {
-        console.log("error, please enter a valid entry.");
+        console.log("Error, please enter a valid entry.");
       }
     });
 }
@@ -86,7 +79,7 @@ function addDepartment() {
       .prompt({
         type: "input",
         name: "addDept",
-        message: "add new department name",
+        message: "New department name?",
       })
       .then((answer) => {
         connect.query("INSERT INTO department SET ?", {
@@ -106,17 +99,17 @@ function addRoles() {
         {
           type: "input",
           name: "addTitle",
-          message: "add new role title",
+          message: "Add new role title.",
         },
         {
           type: "input",
           name: "addSalary",
-          message: "add new role salary",
+          message: "Add new role salary.",
         },
         {
           type: "input",
           name: "addDeptId",
-          message: "add new role department id",
+          message: "Add new role department id.",
           validate: (input) => {
             if (isNaN(input)) {
               console.log("please enter a valid department number.");
@@ -148,32 +141,32 @@ function addEmployee() {
           {
             type: "input",
             name: "fname",
-            message: "Please enter first name",
+            message: "Please enter first name.",
           },
           {
             type: "input",
             name: "lname",
-            message: "please enter last name",
+            message: "Please enter last name.",
           },
           {
             type: "input",
             name: "deptId",
-            message: "please enter department id",
+            message: "Please enter department id.",
           },
           {
             type: "input",
             name: "jobTitle",
-            message: "please enter job title",
+            message: "Please enter job title.",
           },
           {
             type: "input",
             name: "salary",
-            message: "please enter salary",
+            message: "Please enter salary.",
           },
           {
             type: "input",
             name: "manager",
-            message: "please enter reporting manager",
+            message: "Please enter reporting manager.",
           },
         ])
         .then((answer) => {
@@ -192,9 +185,9 @@ function addEmployee() {
 }
 
 function updateEmployeeRole() {
-  const data = `SELECT * FROM employees`;
+  const data = `SELECT DISTINCT * FROM employees`;
   connect.query(data, (err, employee_result) => {
-    connect.query(`SELECT * FROM roles`, (err, title_result) => {
+    connect.query(data, `SELECT * FROM roles`, (err, role_result) => {
       inquirer
         .prompt([
           {
@@ -203,8 +196,7 @@ function updateEmployeeRole() {
             message: "Which employee would you like to update?",
             choices: () =>
               employee_result.map(
-                (employee_result) =>
-                  employee_result.first_name + " " + employee_result.last_name,
+                (employee_result) => employee_result.first_name
               ),
           },
           {
@@ -212,15 +204,20 @@ function updateEmployeeRole() {
             name: "employeeRole",
             message: "Which role do you want to assign the selected employee?",
             choices: () =>
-              title_result.map((title_result) => title_result),
+              role_result.map((role_result) => role_result.job_title),
           },
         ])
         .then((answer) => {
-            console.log('works')
+          const updateSQL = `UPDATE employees SET job_title = ?  WHERE first_name = ?`;
+          const results = [answer.employeeRole, answer.employee];
+          connect.query(updateSQL, results, (req, res) => {
+            console.log("Employee has been updated.");
+          });
+          main();
         });
-      main();
     });
   });
 }
+
 
 main();
